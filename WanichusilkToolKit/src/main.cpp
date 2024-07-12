48ab1fc90a15dfab2488ee174d71d63753cb59be
Ivan-Krul <kiwiivanya128@gmail.com>
2024-07-12 22:32:27 +0200

    trying to come back to sdl2

--- WanichusilkToolKit/src/main.cpp
+++ WanichusilkToolKit/src/main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-
-#include "NewFloatVar/FloatVar.h"
-#include "NewFloatVar/FloatVarNumeric.h"
-#include "NewFloatVar/FloatVarString.h"
-#include <raylib.h>
-
 #include <chrono>
+
+#include "SDL.h"
+
+#undef main
 
 struct TimeStop {
     std::chrono::high_resolution_clock::time_point beg, end;
@@ -23,23 +21,38 @@ struct TimeStop {
 int main(int argc, char* argv[]) {
     printf("Local path: %s\n", argv[0]);
 
-    hardware_envi_lib::FloatVarNumeric fvnf = 0;
-    {
-        hardware_envi_lib::FloatVar fv;
-        fv.name = "What";
-        memcpy(fv.data_type, "flot", 5);
-        fv.setBinaryValue(5.3f);
+    SDL_Window* window = nullptr;
+    SDL_Surface* wnd_surf = nullptr;
 
-        fvnf = fv;
+    if (SDL_Init(SDL_INIT_EVENTS | SDL_INIT_VIDEO) < 0) {
+        fprintf(stderr, "could not initialize sdl2 : % s\n", SDL_GetError());
+        return 1;
     }
-    hardware_envi_lib::FloatVarNumeric fvn = 65;
-    hardware_envi_lib::FloatVarString fvs = "whar";
-    fvs()->name = "no";
 
-    std::cout << "FloatVar: " << fvn()->data_type << " " << fvn()->name << " = " << fvn.getValue<int>() << '\n';
-    std::cout << "FloatVar: " << fvnf()->data_type << " " << fvnf()->name << " = " << fvnf.getValue<float>() << '\n';
-    std::cout << "FloatVar: " << fvs()->data_type << " " << fvs()->name << " = " << fvs.getValue() << '\n';
+    window = SDL_CreateWindow("WanichusilkToolKit - test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 360, SDL_WINDOW_SHOWN);
 
+    if (window == NULL) {
+        fprintf(stderr, "could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    wnd_surf = SDL_GetWindowSurface(window);
+
+    SDL_Event event = { 0 };
+    bool is_quit = false;
+
+    while (!is_quit) {
+        while(SDL_PollEvent(&event))
+            switch (event.type) {
+            case SDL_QUIT: is_quit = true; break;
+            default:
+                break;
+            }
+    }
+
+    SDL_FreeSurface(wnd_surf);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
 
